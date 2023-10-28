d68b9fa94d464e0996ae868b7a91906fe01e6e52
Hengameh M.Khani <afsooneday@gmail.com>
2023-10-28 16:05:02 +0100

    css navbar

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './navbar.css';
+import logo from '../../assets/logo.png';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className='navbar'>
+     <div class="logo"><b>H<span>en</span>g<span>am</span>eh</b></div>
+    
+     <div class="logo1"><b>Full<span>Sta</span>ck<span>Dev</span>eloper</b></div>
+     
+    <div className='desktopMenu'>
+      <Link className= 'desktopMenuListItem'>Home</Link>
+      <Link className= 'desktopMenuListItem'>Projects</Link>
+      <Link className= 'desktopMenuListItem'>About me</Link>
+      <Link className= 'desktopMenuListItem'>Portfolio</Link>
+    </div>
+    <button className='desktopMenuBtn'>
+        <img src='' alt='' className='desktopMenuImg' />Contact Me</button>
+    </nav>
   )
 }
 
